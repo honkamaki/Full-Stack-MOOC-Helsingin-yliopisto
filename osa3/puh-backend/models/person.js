6d459a7b54e999582743498eab54e4e62daaf6c2
RikuHonkamaki <81312875+RikuHonkamaki@users.noreply.github.com>
2025-09-18 16:40:15 +0300

    3.19 testi

--- osa3/puh-backend/models/person.js
+++ osa3/puh-backend/models/person.js
@@ -1,9 +1,16 @@
-// osa3/puh-backend/models/person.js
+// puh-backend/models/person.js
 import mongoose from 'mongoose'
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  number: { type: String, required: true },
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name must be at least 3 characters long'],
+  },
+  number: {
+    type: String,
+    required: [true, 'Number is required'],
+  },
 })
 
 personSchema.set('toJSON', {
@@ -11,7 +18,7 @@ personSchema.set('toJSON', {
     ret.id = ret._id.toString()
     delete ret._id
     delete ret.__v
-  }
+  },
 })
 
 export default mongoose.model('Person', personSchema)
